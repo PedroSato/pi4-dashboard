c247b693abf3a2b7cca20797f4dd14721d31fd0e
Priscyla <priscyla.poliveira@gmail.com>
2019-09-23 04:15:54 -0300

    Criando formulário de cadastro de produtos

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,12 +1,48 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const NewProductWrapper = styled.div`
+    margin-top: 4em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const InputProduct = styled.input`
+    
+`
+
+const LabelProduct = styled.span`
+    
+`
+
+const InputWrapper = styled.div`
+    display:flex;
+    flex-direction:column;
+`
+
+const ColumnWrapper = styled.div`
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+`
 
 const NewProducts = () => {
 
-
     return (
-        <div>
-            <h1>OI</h1>
-        </div>
+        <NewProductWrapper>
+            <ColumnWrapper>
+                <h1>CADASTRO DE PRODUTOS</h1>
+            </ColumnWrapper>
+            
+            <ColumnWrapper>
+                <InputWrapper>
+                    <LabelProduct>Nome do Produto: </LabelProduct>
+                    <InputProduct/>
+                </InputWrapper>
+            </ColumnWrapper>
+        </NewProductWrapper>
+        
     )
 
 }
